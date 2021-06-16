84649943eaebe7877e7fe7ebadadb7060bf75409
premyshan <42987258+premyshan@users.noreply.github.com>
2021-06-16 12:47:42 -0400

    Update srmcollidermetabo.py

--- srmcollidermetabo.py
+++ srmcollidermetabo.py
@@ -264,7 +264,7 @@ Output: compounds list with added columns of 'USI1' and 'Average Interference', 
 def profile_specific(compounds_filt, spectra_filt, mol_id, change = 0, ppm = 0, change_q3 = 0, ppm_q3 = 0, adduct = ['[M+H]+', '[M+Na]+'], col_energy=35, q3 = False, top_n = 0.1, uis_num = 0):
     query, background, uis, interferences, transitions = choose_background_and_query(mol_id = mol_id, change = change, ppm = ppm, change_q3 = change_q3, ppm_q3 = ppm_q3,
                                                                                      col_energy = col_energy,adduct = adduct, q3 = q3, top_n = top_n, spectra_filt = spectra_filt, uis_num=uis_num)
-    return interferences, uis 
+    return query, background, uis, interferences, transitions 
 
 """
 function method_profiler:
